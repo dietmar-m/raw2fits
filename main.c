ebb721b9192223382193652a059c2bbf95689c65
dietmar-m <d.muscholik@t-online.de>
2023-08-14 14:48:45 +0200

    processing of multiple files

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 int usage(char *argv0)
 {
 	fprintf(stderr, "USAGE:\n");
-	fprintf(stderr, "%s <filename>\n",argv0);
+	fprintf(stderr, "%s <filename>...\n",argv0);
 	return 0;
 }
 
@@ -19,58 +19,67 @@ int main(int argc, char **argv)
 	libraw_data_t *rawdata;
 	int flags=0;
 	int err=0;
+	int argn;
 
-	if(argc != 2)
+	if(argc < 2)
 	{
 		usage(argv[0]);
 		return -1;
 	}
+
 	rawdata=libraw_init(flags);
 	if(!rawdata)
 	{
 		fprintf(stderr, "Error initializing Libraw\n");
 		return -1;
 	}
-	err=libraw_open_file(rawdata,argv[1]);
-	if(!err)
+
+	for(argn=1; argn<argc; argn++)
 	{
-		char outname[NAME_MAX];
-		char *p;
-		char *filters[]={"R","G","B",NULL};
-		ssize_t n;
-		fitsfile *outfile[3];
-
-		print_header(rawdata);
-		p=strrchr(argv[1],'.');
-		if(p)
-			*p=0;
-
-		for(n=0; n<3; n++)
+		err=libraw_open_file(rawdata,argv[argn]);
+		if(!err)
 		{
-			sprintf(outname, "!%s-%s.fits", argv[1], filters[n]);
-			//printf("%s\n",outname);
-			if(fits_create_file(&outfile[n], outname, &err))
+			char outname[NAME_MAX];
+			char *p;
+			char *filters[]={"R","G","B",NULL};
+			ssize_t n;
+			fitsfile *outfile[3];
+
+			print_header(rawdata);
+			p=strrchr(argv[1],'.');
+			if(p)
+				*p=0;
+
+			for(n=0; n<3; n++)
 			{
-				fits_report_error(stderr, err);
-				break;
+				sprintf(outname, "!%s-%s.fits", argv[argn], filters[n]);
+				//printf("%s\n",outname);
+				if(fits_create_file(&outfile[n], outname, &err))
+				{
+					fits_report_error(stderr, err);
+					break;
+				}
 			}
-		}
 
-		if(n==rawdata->idata.colors)
-		{
-			err=libraw_unpack(rawdata);
-			if(!err)
-				raw2fits(rawdata,filters,outfile);
-			else
-				fprintf(stderr,"%s\n",libraw_strerror(err));
-		}
+			if(n==rawdata->idata.colors)
+			{
+				err=libraw_unpack(rawdata);
+				if(!err)
+					raw2fits(rawdata,filters,outfile);
+				else
+					fprintf(stderr,"%s\n",libraw_strerror(err));
+			}
 
-		for(n--; n>=0; n--)
-			fits_close_file(outfile[n],&err);
-	}
-	else
-		fprintf(stderr, "%s\n", libraw_strerror(err));
+			for(n--; n>=0; n--)
+				fits_close_file(outfile[n],&err);
+		}
+		else
+			fprintf(stderr, "%s\n", libraw_strerror(err));
 	
+		libraw_recycle(rawdata);
+	}
+
 	libraw_close(rawdata);
+
 	return err;
 }
